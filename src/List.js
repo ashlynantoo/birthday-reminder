3826128a85537a993de8c91c3097abd2c8b96976
Ashlyn Antoo <ashlynantoo@gmail.com>
2026-06-24 16:13:37 -0500

    Change image alt

--- src/List.js
+++ src/List.js
@@ -5,7 +5,7 @@ const List = (props) => {
         const { id, name, age, image } = person;
         return (
           <article key={id} className="person">
-            <img src={image} alt="name"></img>
+            <img src={image} alt={name}></img>
             <div>
               <h4>{name}</h4>
               <p>{age} years</p>
